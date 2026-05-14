9e3bd49ab705926bd2bbaf40f3df524794176a6d
ZhouChengguo <1980800130@qq.com>
2026-05-14 13:49:55 +0800

    feat(shop): 添加商铺类型Redis缓存功能 - 创建了 IShopTypeService 接口并定义查询方法 - 实现了 ShopTypeService 服务层缓存逻辑 - 添加了 ShopTypeController 控制器提供 REST 接口 - 集成了 Redis 缓存提高查询性能 - 在 RedisConstants 中增加了商铺类型相关常量

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,21 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOPTYPE_KEY;
 
 /**
  * <p>
@@ -16,5 +27,30 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result queryTypeList() {
+        // 1.从redis中查询商铺类型的缓存
+        String key = CACHE_SHOPTYPE_KEY;
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(key);
+        // 2.判断缓存是否存在
+        if (StrUtil.isNotBlank(shopTypeJson)){
+            // 3.存在，直接返回
+            List<ShopType> shopTypeList = JSONUtil.toList(shopTypeJson, ShopType.class);
+            return Result.ok(shopTypeList);
+        }
+
+        // 4.不存在，查数据库，根据sort字段升序排列商铺类型
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+        // 5.查询结果不存在，返回错误
+        if (typeList.isEmpty()){
+            return Result.fail("商铺类型不存在！");
+        }
+        // 6.存在，写入redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList));
+        // 7。返回
+        return Result.ok(typeList);
+    }
 }
