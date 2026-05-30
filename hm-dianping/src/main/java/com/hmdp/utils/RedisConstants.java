aae78a7c3e30eb75bf0716440c74172f89abd7e0
ZhouChengguo <1980800130@qq.com>
2026-05-30 14:25:55 +0800

    feat(blog): 实现博客模块的发布探店笔记、查看探店笔记、点赞功能与排行榜相关功能

--- hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
@@ -4,7 +4,8 @@ public class RedisConstants {
     public static final String LOGIN_CODE_KEY = "login:code:";
     public static final Long LOGIN_CODE_TTL = 2L;
     public static final String LOGIN_USER_KEY = "login:token:";
-    public static final Long LOGIN_USER_TTL = 30L;
+    // TODO 为了测试方便，这里将30L
+    public static final Long LOGIN_USER_TTL = 1440L;
 
     public static final Long CACHE_NULL_TTL = 2L;
 
